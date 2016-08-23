ccd3c3db31994a25fdb9e613908c8a5d72aa4c9b
Constance Toulemonde <toulemonde.const@gmail.com>
2016-08-23 19:16:14 +0200

    recipe show

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,11 +27,11 @@ garlic =  Ingredient.create(name: "garlic")
 lemon =  Ingredient.create(name: "lemons")
 
 Houmous  = Recipe.create(name: "Houmous with peanuts", category: "starter", photo: "houmous.png", description: "Hummus is this delicious mashed chickpeas, blended with tahini, the sesame paste. Hummus is native to the Middle East countries. It is eaten raw, with raw vegetables, or with pita bread!", price:3, prep_time:6 , cooking_time:0, calories:150,
-vegetarian: true, gluten_free: true, egg_free: true, dairy_free: true, vegan: true)
+vegetarian: true, gluten_free: true, egg_free: true, dairy_free: true, vegan: true, instructions: "qrebvljhsvgzhKEBJQNVLCHIOGUYezhjkbnv, ;sc,lqzjiezemkqflnbjdfnsvd.")
 
-Dose.create(measure: "400g of", ingredient:chickpea, recipe: Houmous)
-Dose.create(measure: "100g of", ingredient:peanut, recipe: Houmous)
-Dose.create(measure: "dessert spoon of", ingredient: sesame_oil, recipe: Houmous)
-Dose.create(measure: "10g of", ingredient: sesame_pastry, recipe: Houmous)
-Dose.create(measure: "some", ingredient: garlic, recipe: Houmous)
-Dose.create(measure: "2" , ingredient: lemon, recipe: Houmous)
+chick = Dose.create(measure: "400g of", ingredient:chickpea, recipe: Houmous)
+peanut = Dose.create(measure: "100g of", ingredient:peanut, recipe: Houmous)
+sesame = Dose.create(measure: "dessert spoon of", ingredient: sesame_oil, recipe: Houmous)
+pastry = Dose.create(measure: "10g of", ingredient: sesame_pastry, recipe: Houmous)
+garlic = Dose.create(measure: "some", ingredient: garlic, recipe: Houmous)
+lemon = Dose.create(measure: "2" , ingredient: lemon, recipe: Houmous)
