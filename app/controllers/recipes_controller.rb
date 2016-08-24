65ddff2e4d320bb14fc5701eae3bf31257cd9a0e
Constance Toulemonde <toulemonde.const@gmail.com>
2016-08-24 12:18:01 +0200

    starting food preferences filter

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,7 +6,15 @@ class RecipesController < ApplicationController
   end
 
   def weekly
-    @recipes = Recipes.all
+    if user_signed_in?
+      @recipes = Recipe.all
+    else
+      if current_user.has_user_completed_food_preferences?
+        @recipes = Recipe.where(gluten_free: current_user.no_gluten, egg_free: current_user.no_eggs,
+        vegetarian: current_user.vegetarian, vegan: current_user.vegan)
+      else
+        @recipes = Recipe.all
+      end
+    end
   end
-
 end
