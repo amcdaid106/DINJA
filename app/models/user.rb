a40d854008b434774325b79c578ef8a9dd365fb9
Constance Toulemonde <toulemonde.const@gmail.com>
2016-08-24 16:34:10 +0200

    first attempt facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, omniauth_providers: [:facebook]
+
   has_many :orders
   has_many :banished_ingredients
 
@@ -11,7 +13,27 @@ class User < ApplicationRecord
   validates :last_name, presence: true
 
 
+ def self.find_for_facebook_oauth(auth)
+    user_params = auth.to_h.slice(:provider, :uid)
+    user_params.merge! auth.info.slice(:email, :first_name, :last_name)
+    user_params[:token] = auth.credentials.token
+    user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
+
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    user ||= User.where(email: auth.info.email).first # User did a regular sign up in the past.
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.save
+    end
+
+    return user
+end
+
   def has_user_completed_food_preferences?
     !vegetarian.nil?
   end
+
 end
