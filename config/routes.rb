2b8e7e88b3dbad3d4c818d6e1b12d261b49f2f54
Andrea Cavallo <A.Cavallo1@newcastle.ac.uk>
2016-08-25 09:36:15 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  root to: 'pages#home'
-
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "recipes#weekly"
